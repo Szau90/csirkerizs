d456582dcdd4fd720476cfb4e7d42c91226e8e4f
Szau90 <szauer.d@gmail.com>
2023-10-27 16:09:32 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <header>
+    <p>header</p>
+  </header>
+  <main>
+    <router-view></router-view>
+  </main>
+  <footer>
+    <p>footer</p>
+  </footer>
+</template>
+
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,4 @@
+<template>
+
+    <p class="text-red-600 font-extrabold text-[69px]">HOME PAGE</p>
+</template>
